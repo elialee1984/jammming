8858726d30d2cad0c9a8bd3b76a07fd7d31f76d3
Elia Lee <elia.lee1984@gmail.com>
2024-05-31 19:09:19 +0200

    Renaming playlist and saving it to a user's account enabled

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Tracklist from "./components/Tracklist/Tracklist";
 
 const App = () => {
     const [tracks, setTracks] = useState([
-        { id: 1, name: "Track 1", artist: "Artist 1", album: "Album 1" },
-        { id: 2, name: "Track 2", artist: "Artist 2", album: "Album 2" },
-        { id: 3, name: "Track 3", artist: "Artist 3", album: "Album 3" },
-        { id: 4, name: "Track 4", artist: "Artist 4", album: "Album 4" },
-        { id: 5, name: "Track 5", artist: "Artist 5", album: "Album 5" },
+        { id: 1, name: "Track 1", artist: "Artist 1", album: "Album 1", uri: "spotify:track:1" },
+        { id: 2, name: "Track 2", artist: "Artist 2", album: "Album 2", uri: "spotify:track:2" },
+        { id: 3, name: "Track 3", artist: "Artist 3", album: "Album 3", uri: "spotify:track:3" },
+        { id: 4, name: "Track 4", artist: "Artist 4", album: "Album 4", uri: "spotify:track:4" },
+        { id: 5, name: "Track 5", artist: "Artist 5", album: "Album 5", uri: "spotify:track:5" },
     ]);
     const [playlistName, setPlaylistName] = useState("My Playlist");
     const [playlistTracks, setPlaylistTracks] = useState(tracks.slice(0, 1));
@@ -29,10 +29,21 @@ const App = () => {
         );
     };
 
+    const handleNameChange = newName => {
+        setPlaylistName(newName);
+    };
+
+    const savePlaylist = () => {
+        const trackURIs = playlistTracks.map(track => track.uri);
+        console.log("Saving playlist with URIs:", trackURIs); // Simulate saving to Spotify
+        setPlaylistTracks([]); // Reset the playlist
+        setPlaylistName("New Playlist"); // Reset the playlist name
+    };
+
     return (
         <div>
             <h2>Playlist-Component</h2>
-            <Playlist name={playlistName} tracks={playlistTracks} onAdd={addTrack} onRemove={removeTrack}/>
+            <Playlist name={playlistName} tracks={playlistTracks} onRemove={removeTrack} onNameChange={handleNameChange} onSave={savePlaylist}/>
             <h2>SearchBar-Component</h2>
             <SearchBar />
             <h2>SearchResuls-Component</h2>
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Tracklist from "../Tracklist/Tracklist";
 
-const Playlist = ({ name, tracks, onRemove }) => {
+const Playlist = ({ name, tracks, onRemove, onNameChange, savePlaylist }) => {
     return (
         <div>
-            <h2>{name}</h2>
+            <input
+                type="text"
+                value={name}
+                onChange={e => onNameChange(e.target.value)}
+                style={{ fontSize: '24px', fontWeight: 'bold' }}
+            />
             <Tracklist tracks={tracks} onRemove={onRemove} />
+            <button onClick={savePlaylist}>Save to Spotify</button>
         </div>
     );
 };
